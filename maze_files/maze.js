6df404447937ecdb8c09676dd2e98325356bd152
fissehawel <fissehaweb@gmail.com>
2019-04-13 22:58:26 -0500

    lab8

--- maze_files/maze.js
+++ maze_files/maze.js
@@ -22,7 +22,7 @@ $(function(){
             if (flag) {
                 $("#status").text("You Win!");
                 flag = false;
-                setTimeout(tryAgain, 5000);
+                setTimeout(tryAgain, 2500);
             }
         });
     });
@@ -30,7 +30,7 @@ $(function(){
 function youLose(){
     $("#maze .boundary").css("background-color", "red");
     $("#status").text("You Lose!");
-    setTimeout(tryAgain, 5000);
+    setTimeout(tryAgain, 2500);
 }
 function tryAgain(){
     $("#status").text('Click the \"S\" to begin');
